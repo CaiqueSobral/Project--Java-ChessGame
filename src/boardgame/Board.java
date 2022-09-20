b86f7871b11d0e2e37cce65876df4dd73c6d5dd2
Caique Sobral <caiquelsobral@gmail.com>
2022-09-20 15:54:57 -0300

    FEAT: print board and create chess layer

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,9 +6,9 @@ public class Board {
 
     private Piece[][] pieces;
 
-    public Board() {
-        this.rows = 8;
-        this.columns = 8;
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
 
         pieces = new Piece[rows][columns];
     }
@@ -27,5 +27,13 @@ public class Board {
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
